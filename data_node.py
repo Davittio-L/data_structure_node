76f168c02b81b5e0f884fa7328f259d8626a61e3
colten <cbayles@outlook.com>
2023-10-31 13:24:26 -0400

    Added Getters - .get_value() & .get_link_node()

--- data_node.py
+++ data_node.py
@@ -5,3 +5,9 @@ class Node:
     def __init__(self, value, link_node=None):
         self.value = value
         self.link_node = link_node
+    
+    def get_value(self):
+        return self.value
+    
+    def get_link_node9(self):
+        return self.get_link_node
