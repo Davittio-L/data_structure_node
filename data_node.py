87a22d1d62622a672fa708939debc8aa61690d6d
colten <cbayles@outlook.com>
2023-10-31 13:36:01 -0400

    Added Setter - and node definitions.

--- data_node.py
+++ data_node.py
@@ -9,5 +9,21 @@ class Node:
     def get_value(self):
         return self.value
     
-    def get_link_node9(self):
-        return self.get_link_node
+    def get_link_node(self):
+        return self.link_node
+    
+    def set_link_node(self, link_node):
+        self.link_node = link_node
+
+james = Node("likes to snowboard")
+john = Node("likes to ski")
+stephanie = Node("does not like the snow")
+
+stephanie.set_link_node(john)
+james.set_link_node(stephanie)
+
+stephanie_mood = james.get_link_node().get_value()
+john_mood = stephanie.get_link_node().get_value()
+
+print(stephanie_mood)
+print(john_mood)
